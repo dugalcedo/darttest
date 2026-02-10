53e0d66ac638ae3a1ef02fc087974ea8ee1a07ac
dugalcedo <dougalcedo@gmail.com>
2026-02-10 14:36:56 +0100

    first

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() async {
+  var server = await HttpServer.bind(InternetAddress.anyIPv6, 80);
+  
+  await server.forEach((HttpRequest req) {
+    req.response.write("Hello");
+    req.response.close();
+  });
+}

336fd69d29eb604cb6bb69394c347dca44d2a32d
dugalcedo <dougalcedo@gmail.com>
2026-02-10 14:37:38 +0100

    change port

--- main.dart
+++ main.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() async {
-  var server = await HttpServer.bind(InternetAddress.anyIPv6, 80);
+  var server = await HttpServer.bind(InternetAddress.anyIPv6, 4444);
   
   await server.forEach((HttpRequest req) {
     req.response.write("Hello");
